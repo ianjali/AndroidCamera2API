1176bcedb8dc2c29e29e1388e9d3352b6e19d81f
Anjali <15ucc004@lnmiit.ac.in>
2020-04-30 09:31:59 +0530

    first

---
+++ app/src/main/java/com/example/androidcamera2api/MainPage.java
@@ -0,0 +1,44 @@
+package com.example.androidcamera2api;
+
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MainPage extends Activity {
+
+    Button button;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, AndroidCamera.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
+
+}
